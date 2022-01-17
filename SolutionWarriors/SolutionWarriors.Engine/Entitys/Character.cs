3b40cb7bb4a81881bf700de2e6ac164b24f131f4
JoaoMantovani <84257811+0mochileiro@users.noreply.github.com>
2022-01-17 19:08:49 -0300

    João Mantovani - 17012022
    Initial commit - Normalize Solution.

---
+++ SolutionWarriors/SolutionWarriors.Engine/Entitys/Character.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SolutionWarriors.Engine.Entitys
+{
+    public class Character
+    {
+        public int CharacterId { get; set; }
+        public string CharacterName { get; set; }
+        public string CharacterTitle { get; set; }
+        public CharacterClass CharacterCategory { get; set; }
+        public int CharacterLife { get; set; }
+        public int CharacterAmor { get; set; }
+        public int CharacterAttackPower { get; set; }
+        public string CharacterSpecialPower { get; set; }
+
+    }
+
+    public enum CharacterClass
+    {
+        Barbarian = 0,
+        Bard = 1,
+        Wizard = 2,
+        Cleric = 3,
+        Druid = 4,
+        Sorcerer = 5,
+        Warrior = 6,
+        Rouge = 7,
+        Mage = 8,
+        Monk = 9,
+        Paladin = 10
+    }
+}
---
+++ SolutionWarriors/SolutionWarriors.Engine/Entitys/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SolutionWarriors.Engine.Entitys
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string UserNickname { get; set; }
+        public string UserEmail { get; set; }
+        public int UserAcessLevel { get; set; }
+        public byte[] UserPasswordHash { get; set; }
+        public byte[] UserPasswordSalt { get; set; }
+        public DateTime UserRegisterDate { get; set; }
+        public DateTime UserLastUpdate { get; set; }
+    }
+
+    public enum UserAcessLevel
+    {
+        Player = 0,
+        Administrator = 1,
+        Root = 2
+    }
+}
