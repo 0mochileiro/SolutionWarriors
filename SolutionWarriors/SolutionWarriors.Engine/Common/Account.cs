29b54d80c175068ca578712a1b77fc282508fcc7
JoaoMantovani <84257811+0mochileiro@users.noreply.github.com>
2022-01-18 01:26:50 -0300

    João Mantovani - 18012022
    SolutionWarriors.Engine - Account classe

---
+++ SolutionWarriors/SolutionWarriors.Engine/Common/Account.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SolutionWarriors.Engine.Common
+{
+    public class Account
+    {
+        public (byte[] hash, byte[] salt) GenerateHMACSHA512Hash(string password)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                var salt = hmac.Key;
+
+                var hash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+                return (hash, salt);
+            }
+        }
+
+        public bool VerifyPasswordHash(string password, byte[] storedHash, byte[] storedSalt)
+        {
+            if (password == null) throw new ArgumentNullException("password");
+            if (string.IsNullOrWhiteSpace(password)) throw new ArgumentException("Value cannot be empty or whitespace only string.", "password");
+            if (storedHash.Length != 64) throw new ArgumentException("Invalid length of password hash (64 bytes expected).", "passwordHash");
+            if (storedSalt.Length != 128) throw new ArgumentException("Invalid length of password salt (128 bytes expected).", "passwordHash");
+
+            using (var hmac = new HMACSHA512(storedSalt))
+            {
+                var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+                return computedHash.SequenceEqual(storedHash);
+            }
+        }
+    }
+}
